b4d56977ab85c64c86284bef3e00bc719efb4e7a
Miguel <mmedin52@calpoly.edu>
2024-10-27 16:39:41 -0700

    initial commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+ function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background(220);
+for (i = 0; i<10; i++) { 
+  rect(random(width),random(height), 50,50)
+}
+
+  
+
+
+
+}
